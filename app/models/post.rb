572c1c11f2f40427071db9df27bffa58a6c52775
Jamie <bennythedev@gmail.com>
2022-08-11 11:39:31 -0500

    commit tree d095cdff816c821e69fe526eee5819d12f4ae43f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { :in => ['Fiction', 'Non-Fiction']}
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        ["Won't Believe", "Secret", "Top [number]", "Guess"].map do |clickbait|
+            if title&.include?(clickbait)
+                return true
+            end
+        end
+
+        errors.add(:title, 'title is not clickbait')           
+    end
+
 end
